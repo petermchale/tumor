df100b52ad5fc478c4d417ef15d464f7265d5c05
Peter McHale <ccbs@dhcp-7-142.bio.uci.edu>
2016-09-16 15:38:07 -0700

    minor

--- tumor_package/animate.py
+++ tumor_package/animate.py
@@ -2,7 +2,6 @@
 
 from matplotlib import pyplot as plt
 from matplotlib import animation
-import numpy as np
 
 from read import read_into_dict
 from tumor import Tumor
@@ -112,47 +111,6 @@ def animate_tumor_growth(number_of_frames, random_seed=None, run_mode='plot anim
         exit()
 
 
-def generate_tumor_growth_trajectories_base(initialCondition, parameterValues, number_realizations, random_seed=None, output_directory_name='./'):
-    """
-    Generate many time courses of tumor growth and save data
-    """
-
-    prng = np.random.RandomState(random_seed)
-    random_seed_array = prng.randint(0, 1000, number_realizations)
-    number_of_states = int(parameterValues['number_of_states'])
-    time_points = -np.ones((number_realizations, number_of_states), dtype=float)
-    number_C_cells = -np.ones((number_realizations, number_of_states), dtype=int)
-    number_Q_cells = -np.ones((number_realizations, number_of_states), dtype=int)
-    from write import cleanUp_createFile, append_log_file
-    flog = cleanUp_createFile(output_directory_name + 'tumor.log', 'a')
-    for realization in range(number_realizations):
-        tumor = Tumor(initialCondition, parameterValues, random_seed=random_seed_array[realization])
-        time_course = tumor.generate_time_course()
-        time_points[realization, :] = time_course.time_points
-        number_C_cells[realization, :] = time_course.number_C_cells
-        number_Q_cells[realization, :] = time_course.number_Q_cells
-        append_log_file(flog, realization, time_course)
-    print('finished generating time courses\n')
-
-    # type '!unzip -l data.npz' in the Python Console to see individual files in zip archive:
-    np.savez(output_directory_name + parameterValues['data_file_name'],
-             time_points=time_points,
-             number_C_cells=number_C_cells,
-             number_Q_cells=number_Q_cells)
-
-
-def generate_tumor_growth_trajectories(number_realizations, random_seed=None):
-    """
-    Read in initial conditions & parameter values, and generate many time courses of tumor growth and save data
-    """
-
-    initialCondition = read_into_dict('initialCondition.in')
-    parameterValues = read_into_dict('parameterValues.in')
-
-    generate_tumor_growth_trajectories_base(initialCondition, parameterValues, number_realizations, random_seed)
-
-
 if __name__ == '__main__':
 
-    # animate_tumor_growth(number_of_frames=400, random_seed=2, run_mode='plot animation')
-    generate_tumor_growth_trajectories(number_realizations=1, random_seed=2)
+    animate_tumor_growth(number_of_frames=400, random_seed=2, run_mode='plot animation')
