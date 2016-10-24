a1b864b31cadbc4035d89bd2aa059cd79daf5ac9
Guillaume Gay <guillaume@damcb.com>
2016-10-24 10:04:29 +0200

    Simplifies sum_cell_cell_energies

--- tumor_package/energy.py
+++ tumor_package/energy.py
@@ -5,31 +5,42 @@ from scipy.spatial.distance import pdist
 
 
 def sum_cell_cell_energies(cell_cell_distances):
-    """
-    Given a list of cell-cell distances, compute the corresponding cell-cell interaction energies and sum those cell-cell interaction energies.
-    Cell-cell interaction energy is strongly repulsive at short distances, weakly attractive at intermediate distances, and zero at long distances
+    """Given a list of cell-cell distances, compute the corresponding
+
+    cell-cell interaction energies and sum those cell-cell interaction
+    energies.  Cell-cell interaction energy is strongly repulsive at
+    short distances, weakly attractive at intermediate distances, and
+    zero at long distances
     """
 
-    Va = 120
-    Vc = 12
+    Va = 120.
+    Vc = 12.
     aa = 1
     bb = 1.2
     cc = 2
     nn = 2
 
-    cond1 = (cell_cell_distances < aa)
-    energy1 = Va * np.sum(np.power(aa / cell_cell_distances[cond1], nn)) - len(cell_cell_distances[cond1]) * float(Vc)
-    cond2 = (~(cell_cell_distances < aa)) & (cell_cell_distances < bb)
-    energy2 = -len(cell_cell_distances[cond2]) * float(Vc)
-    cond3 = (~(cell_cell_distances < bb)) & (cell_cell_distances < cc)
-    energy3 = Vc * np.sum(cell_cell_distances[cond3] - bb) / (cc - bb) - len(cell_cell_distances[cond3]) * float(Vc)
+    cc_dist1 = cell_cell_distances[(cell_cell_distances < aa)]
+    energy1 = Va * np.sum(np.power(aa / cc_dist1, nn))
+    energy1 -= cc_dist1.size * Vc
+
+    cc_dist2 = cell_cell_distances[(~(cell_cell_distances < aa)) &
+                                   (cell_cell_distances < bb)]
+    energy2 = -cc_dist2.size * Vc
+
+    cc_dist3 = cell_cell_distances[(~(cell_cell_distances < bb)) &
+                                   (cell_cell_distances < cc)]
+
+    energy3 = Vc * np.sum(cc_dist3 - bb) / (cc - bb)
+    energy3 -= cc_dist3.size * Vc
 
     return energy1 + energy2 + energy3
 
 
 def calculate_tumor_energy(cells):
-    """
-    Brute-force computation of tumor energy by summing the cell-cell interaction energies of all unique cell pairs
+    """Brute-force computation of tumor energy by summing the cell-cell
+
+    interaction energies of all unique cell pairs
     """
 
     if len(cells) == 0:
@@ -39,5 +50,6 @@ def calculate_tumor_energy(cells):
         return 'undefined'
     else:
         cell_positions = np.array([cell.position for cell in cells])
-        cell_cell_distances = pdist(cell_positions)  # much faster than looping over all unique cell pairs
+        # much faster than looping over all unique cell pairs
+        cell_cell_distances = pdist(cell_positions)
         return sum_cell_cell_energies(cell_cell_distances)
