3c04ca4c56b4e4e6f4b035cb44798dc871eab1f7
Peter McHale <peter.thomas.mchale@gmail.com>
2016-11-26 09:04:29 -0800

    numexpr

--- tumor_package/energy.py
+++ tumor_package/energy.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from scipy.spatial.distance import pdist
+import numexpr as ne
 
 
 def sum_cell_cell_energies(cell_cell_distances):
@@ -19,21 +20,25 @@ def sum_cell_cell_energies(cell_cell_distances):
     cc = 2
     nn = 2
 
-    cc_dist1 = cell_cell_distances[(cell_cell_distances < aa)]
-    energy1 = Va * np.sum(np.power(aa / cc_dist1, nn))
-    energy1 -= cc_dist1.size * Vc
+    cc_dist1 = cell_cell_distances[ne.evaluate('cell_cell_distances < aa')]
+    # putting sum outside ne.evaluate (instead of inside it) handles the case when cc_dist1 is empty
+    energy1 = np.sum(ne.evaluate('Va * (aa / cc_dist1)**nn'))
+    cc_dist1_size = cc_dist1.size
+    energy1 = ne.evaluate('energy1 - cc_dist1_size * Vc')
 
-    cc_dist2 = cell_cell_distances[(~(cell_cell_distances < aa)) &
-                                   (cell_cell_distances < bb)]
-    energy2 = -cc_dist2.size * Vc
+    cc_dist2 = cell_cell_distances[
+        ne.evaluate('(~(cell_cell_distances < aa)) & (cell_cell_distances < bb)')]
+    cc_dist2_size = cc_dist2.size
+    energy2 = ne.evaluate('-cc_dist2_size * Vc')
 
-    cc_dist3 = cell_cell_distances[(~(cell_cell_distances < bb)) &
-                                   (cell_cell_distances < cc)]
+    cc_dist3 = cell_cell_distances[
+        ne.evaluate('(~(cell_cell_distances < bb)) & (cell_cell_distances < cc)')]
+    # putting sum outside ne.evaluate handles the case when cc_dist3 is empty
+    energy3 = np.sum(ne.evaluate('Vc * (cc_dist3 - bb) / (cc - bb)'))
+    cc_dist3_size = cc_dist3.size
+    energy3 = ne.evaluate('energy3 - cc_dist3_size * Vc')
 
-    energy3 = Vc * np.sum(cc_dist3 - bb) / (cc - bb)
-    energy3 -= cc_dist3.size * Vc
-
-    return energy1 + energy2 + energy3
+    return ne.evaluate('energy1 + energy2 + energy3')
 
 
 def calculate_tumor_energy(cells):
